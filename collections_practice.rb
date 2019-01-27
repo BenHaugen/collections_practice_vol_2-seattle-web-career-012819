c84d0585bdeb9d2375f4193b3b45ded1c72a8cbc
Ben Haugen <ben1232jazz@hotmail.com>
2019-01-27 18:10:43 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,57 @@
-# your code goes here
+require "pry"
+
+def begins_with_r(array)
+  array.all? do |word|
+    word.start_with?("r")
+  end    
+end 
+
+   
+def contain_a(array)
+  array.select do |word|
+    word.include?("a")
+  end
+end
+    
+ def first_wa(array)
+   array.find do |word|
+     word[0..1] == ("wa")
+   end
+ end 
+ 
+ def remove_non_strings(array)
+   array.delete_if do |element|
+     element.is_a?(String) == false
+   end
+ end
+ 
+ def count_elements(array)
+ end 
+ 
+ 
+ 
+ def find_cool(array) 
+   array.select do |hashes|
+     hashes.has_value?("cool")
+   end
+ end
+ 
+ def organize_schools(schools)
+   organized_schools = {}
+   schools.each do |school, data|
+     data.each do |key, city|
+       if organized_schools[city] == nil 
+         organized_schools[city] = [school]
+       else 
+         organized_schools[city] << school
+    end
+   end
+  end
+  organized_schools
+ end
+   
+   
+   
+   
+   
+   
